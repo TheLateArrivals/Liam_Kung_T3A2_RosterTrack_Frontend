eaf4e571640c546619f47c4b8effc6d0f96710f9
double0kung <00kung@gmail.com>
2023-07-20 23:43:44 +1000

    Landing Page
    Styling in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
-import React from 'react'; // Imports the React library
+import React from 'react';
+import './App.css';
 
-// Defines a functional component called App
 function App() {
-  // Returns the JSX that should be rendered by this component
-  return (//This is what will be displayed on the screen
-    <div className="App">
-      <h1>ROSTER TRACK</h1>
+  return (
+    <div className="app">
+      <header className="header">
+        {/* Header content goes here */}
+      </header>
+      <div className="main-content">
+        <div className="left-section">
+          <div className="logo-title">
+            <img src="logo.png" alt="RosterTrack Logo" className="logo" />
+            <h1 className="title">RosterTrack</h1>
+          </div>
+        </div>
+        <div className="right-section">
+          <h2 className="main-heading">Simplifying team scheduling.</h2>
+          <p className="sub-heading">Transform your rostering experience today.</p>
+          <div className="sign-in-section">
+            {/* Your sign-in form goes here */}
+          </div>
+        </div>
+      </div>
+      <footer className="footer">
+        {/* Footer content goes here */}
+      </footer>
     </div>
   );
 }
 
-export default App; // Exports the App component so it can be imported in other files
+export default App;
