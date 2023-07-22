a38a16c227d130c79b3cc47dbc1ed600f79afb2a
double0kung <00kung@gmail.com>
2023-07-23 04:34:35 +1000

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,15 +3,20 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function Register() {
+  // State variables to store user input for username, email, and password
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  // State variables for success and error messages
   const [showSuccess, setShowSuccess] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
+  // Function to handle the sign-up form submission
   const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Prevent the default form submission behaviour ie a full page reload
 
+    // Create an object with the user's username, email, and password data
     const userData = {
       username: username,
       email: email,
@@ -19,12 +24,15 @@ function Register() {
     };
 
     try {
+      // Make a POST request to the backend API endpoint '/users/register'
+      // Send 'userData' to the server for registration
       const response = await axios.post('http://localhost:8080/users/register', userData);
-      const token = response.data;
-      console.log('Received token:', token);
-      setShowSuccess(true);
+      const token = response.data; // Get the token from the response data received from the server
+      console.log('Received token:', token); // Log the received token to the console
+      setShowSuccess(true); // Display success message on successful registration
       setErrorMessage(''); // Clear any previous error message on successful registration
     } catch (error) {
+      // Handle errors that occurred during registration
       if (error.response) {
         console.error('Error Response:', error.response.data);
         setErrorMessage(error.response.data.data || 'An error occurred during registration.');
@@ -40,7 +48,7 @@ function Register() {
 
   return (
     <div>
-      {showSuccess ? (
+      {showSuccess ? ( // If registration was successful, display success message
         <div>
           <h2>Registration Successful!</h2>
           <p>Congratulations! You have successfully signed up.</p>
@@ -53,26 +61,26 @@ function Register() {
             type="text"
             placeholder="Username"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)} // Update 'username' state with user input
           />
           {/* Email input field */}
           <input
             type="email"
             placeholder="Email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => setEmail(e.target.value)} // Update 'email' state with user input
           />
           {/* Password input field */}
           <input
             type="password"
             placeholder="Password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => setPassword(e.target.value)} // Update 'password' state with user input
           />
           {/* "Sign Up" button */}
           <button type="submit">Sign Up</button>
 
-          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>} {/* Display error message if present */}
         </form>
       )}
     </div>
