97161c1166b8f296b6e320039b57c3c0398dce55
double0kung <00kung@gmail.com>
2023-07-26 14:57:16 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-axios.defaults.baseURL = 'https://frontendlocaltest.00kung.workers.dev/';
+axios.defaults.baseURL = 'process.env.REACT_APP_API_BASE_URL';
