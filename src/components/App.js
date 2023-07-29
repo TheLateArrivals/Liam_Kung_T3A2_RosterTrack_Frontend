c3cb7fa520c479b03cdb21a823c374e722eb6370
yops320 <liamdavidson4@gmail.com>
2023-07-29 13:56:27 +0930

    Login Page
    Created Login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 // App.js
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './Register'; // Import the Register component
+import Login from './Login';
 import logo from '../docs/logo.png'; // Import the logo.png file
 
 function App() {
@@ -12,26 +14,31 @@ function App() {
         {/* Header content goes here */}
       </header>
       {/* Main content section */}
-      <div className="main-content">
-        {/* Left section */}
-        <div className="section-1">
-          <div className="logo-title">
-            {/* Your logo image goes here */}
-            <img src={logo} alt="RosterTrack Logo" className="logo" />
-            {/* Your app title goes here */}
-            <h1 className="title">RosterTrack</h1>
+      <Router> {/* Wrap your content with Router */}
+        <div className="main-content">
+          {/* Left section */}
+          <div className="section-1">
+            <div className="logo-title">
+              {/* Your logo image goes here */}
+              <img src={logo} alt="RosterTrack Logo" className="logo" />
+              {/* Your app title goes here */}
+              <h1 className="title">RosterTrack</h1>
+            </div>
+          </div>
+          {/* Right section */}
+          <div className="section-2">
+            {/* Introduction text */}
+            <h2 className="intro">Simplifying team scheduling. Transform your rostering experience today.</h2>
+            {/* Sign-up and Login forms */}
+            <div className="sign-up-form">
+              <Routes>
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </div>
           </div>
         </div>
-        {/* Right section */}
-        <div className="section-2">
-          {/* Introduction text */}
-          <h2 className="intro">Simplifying team scheduling. Transform your rostering experience today.</h2>
-          {/* Sign-up form */}
-          <div className="sign-up-form">
-            <Register /> {/* Render the Register component here */}
-          </div>
-        </div>
-      </div>
+      </Router>
       {/* Your app's footer goes here (if needed) */}
       <footer className="footer">
         {/* Footer content goes here */}
@@ -39,5 +46,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {      
+      const response = await axios.post('/users/login', {
+        username,
+        password,
+      });
+
+    } catch (error) {
+      setErrorMessage('Invalid username or password');
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label>Username:</label>
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default Login;
