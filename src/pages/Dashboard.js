773b753c6ece110f122091976185f04146ec5923
yops320 <liamdavidson4@gmail.com>
2023-07-29 20:41:11 +0930

    post login page
    User dash

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className="dashboard">
+      <h2>Welcome to Your Dashboard</h2>
+      <p>Something should go here.</p>
+    </div>
+  );
+}
+
+export default Dashboard;
