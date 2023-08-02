5b2de6c913111413880805bf1faa4ad7d64515d6
double0kung <00kung@gmail.com>
2023-08-02 20:52:58 +1000

    Updated Dashboard to display user

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
-const Dashboard = () => {
-    return (
-      <div>        
-        <div className="dashboard-content">
-          {/* dashboard Content Goes Here */}
-          <h1>Under Construction</h1>
-        </div>
+const Dashboard = ({ userDetails }) => { // Accept userDetails prop
+  return (
+    <div>        
+      <div className="dashboard-content">
+        {/* dashboard Content Goes Here */}
+        <h1>Under Construction</h1>
+        {userDetails && (
+          <div>
+            <h2>Welcome, {userDetails.name}</h2>
+            {/* You can add more details here */}
+          </div>
+        )}
       </div>
-    );
-  };
+    </div>
+  );
+};
 
 export default Dashboard;
