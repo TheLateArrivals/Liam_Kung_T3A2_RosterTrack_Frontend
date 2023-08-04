c550e4f8715faf3f7ae31e9ba6d58dfd59ca453e
yops320 <liamdavidson4@gmail.com>
2023-08-05 01:30:01 +0930

    filter by employee
    filter option

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -11,25 +11,48 @@ const Dashboard = () => {
     endTime: '',
     location: '',
   });
+  const [filteredEmployee, setFilteredEmployee] = useState('');
+  const [filteredShifts, setFilteredShifts] = useState([]);
 
   useEffect(() => {
     // Fetch shifts from the backend
     axios.get('/shifts') 
       .then(response => {
         setShifts(response.data);
+        setFilteredShifts(response.data); // Initialize filtered shifts with all shifts
       })
       .catch(error => {
         console.error('Error fetching shifts:', error);
       });
   }, []);
 
+  useEffect(() => {
+    // Filter shifts when the filteredEmployee value changes
+    if (filteredEmployee) {
+      const filtered = shifts.filter(shift => shift.employee_id.includes(filteredEmployee));
+      setFilteredShifts(filtered);
+    } else {
+      setFilteredShifts(shifts);
+    }
+  }, [filteredEmployee, shifts]);
+
+  const handleEmployeeFilterChange = event => {
+    const { value } = event.target;
+    setFilteredEmployee(value);
+  };
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>
       <div className="dashboard-content" style={{ maxWidth: '600px', width: '100%', textAlign: 'center' }}>
+        {/* Employee Filter */}
+        <label>
+          Filter by Employee:
+          <input type="text" value={filteredEmployee} onChange={handleEmployeeFilterChange} />
+        </label>
         {/* Display Shifts */}
         <h1>Shifts</h1>
         <ul style={{ listStyleType: 'none', padding: 0 }}>
-          {shifts.map(shift => (
+          {filteredShifts.map(shift => (
             <li key={shift._id} style={{ margin: '20px 0', borderBottom: '1px solid #ccc', paddingBottom: '10px' }}>
               {/* Display shift details */}
               <strong>Day:</strong> {shift.day}<br />
