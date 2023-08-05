6398a0d44816ba558dfbcd01e60e1dcf6a6432ba
double0kung <00kung@gmail.com>
2023-08-05 23:24:44 +1000

    Adjust styling to login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -32,22 +32,26 @@ const Login = ({ onLogin }) => { // Receive the onLogin prop
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+    <div className="main-content">
+      <div className="section-2">
+        <h2 className="intro">Login to your account</h2>
+        <div className="sign-up-form">
+          <form onSubmit={handleLogin}>
+            <div className="form-group">
+              <label>Username:</label>
+              <input type="text" className="form-control" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <label>Password:</label>
+              <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>
+            <div className="form-group">
+              <button type="submit" className="submit-btn">Login</button>
+            </div>
+          </form>
+          {errorMessage && <p>Sorry, wrong username or password</p>}
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <div>
-          <button type="submit">Login</button>
-        </div>
-      </form>
-      {errorMessage && <p>Sorry, wrong username or password</p>}
+      </div>
     </div>
   );
 };
