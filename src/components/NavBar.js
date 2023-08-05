4a4c5f1a075d64a17021be3d13a10afc7959db0a
double0kung <00kung@gmail.com>
2023-08-05 21:20:33 +1000

    Updated Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../docs/logo.png';
 
 const Navbar = ({ handleLogout }) => {
+  const [open, setOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setOpen(!open);
+  };
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
+        {/* Logo */}
+        <img src={logo} alt="Logo" className="navbar-logo" />
+
         {/* Hamburger Menu Icon for Mobile */}
-        <div className="menu-toggle" id="mobile-menu">
+        <div className="menu-toggle" id="mobile-menu" onClick={handleMenuToggle}>
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
         </div>
 
         {/* Navbar Links */}
-        <ul className="nav-list">
-          <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link to="/scheduler">Scheduler</Link></li>
-          <li><Link to="/my-team">My Team</Link></li>
-          <li><Link to="/message">Message</Link></li>
-          <li><Link to="/shifts">Shifts</Link></li>          
+        <ul className={`nav-list ${open ? 'open' : ''}`}>
+          <li><Link to="/dashboard" onClick={closeMenu}>Dashboard</Link></li>
+          <li><Link to="/scheduler" onClick={closeMenu}>Scheduler</Link></li>
+          <li><Link to="/my-team" onClick={closeMenu}>My Team</Link></li>
+          <li><Link to="/message" onClick={closeMenu}>Message</Link></li>
+          <li><Link to="/shifts" onClick={closeMenu}>Shifts</Link></li>          
         </ul>
       </div>
     </nav>
